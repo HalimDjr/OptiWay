7812ea9a2dac189eab4f27a5ebfb9cddebea3ab0
l3miage-bentahaa <bentahahcene44@outlook.com>
2026-03-09 16:04:31 +0100

    migration

--- client/.angular/cache/21.2.1/projet-data-synthesis/vite/deps/@bluehalo_ngx-leaflet.js
+++ client/.angular/cache/21.2.1/projet-data-synthesis/vite/deps/@bluehalo_ngx-leaflet.js
@@ -1,4 +1,7 @@
 import {
+  require_leaflet_src
+} from "./chunk-GVIK23OT.js";
+import {
   Directive,
   ElementRef,
   EventEmitter,
@@ -19,9 +22,6 @@ import {
   ɵɵresolveWindow
 } from "./chunk-XXTULQMF.js";
 import "./chunk-RSS3ODKE.js";
-import {
-  require_leaflet_src
-} from "./chunk-GVIK23OT.js";
 import {
   __toESM
 } from "./chunk-R327OCYJ.js";
