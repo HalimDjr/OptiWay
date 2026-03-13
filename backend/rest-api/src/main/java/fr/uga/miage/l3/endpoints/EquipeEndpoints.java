183f7abc07967caaf4442499eb1edf14b9a3e284
Celia <celia.bouamara@etu.univ-grenoble-alpes.fr>
2026-03-13 12:36:04 +0100

    Execptions

--- backend/rest-api/src/main/java/fr/uga/miage/l3/endpoints/EquipeEndpoints.java
+++ backend/rest-api/src/main/java/fr/uga/miage/l3/endpoints/EquipeEndpoints.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/// ///
 @RequestMapping("/api/equipes")
 @Tag(name = "Equipes", description = "Gestion des équipes")
 public interface EquipeEndpoints {
