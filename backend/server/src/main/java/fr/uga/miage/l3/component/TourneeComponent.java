30be4b9ab828c7c9510ceddf58e8d8399b295301
l3m-bentahaa <bentahahcene44@outlook.com>
2026-03-15 20:43:07 +0100

    ajout des exceptions pour getCommandesNonLivres

--- backend/server/src/main/java/fr/uga/miage/l3/component/TourneeComponent.java
+++ backend/server/src/main/java/fr/uga/miage/l3/component/TourneeComponent.java
@@ -5,14 +5,24 @@ import fr.uga.miage.l3.models.TourneeEntity;
 
 import fr.uga.miage.l3.repository.TourneeRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class TourneeComponent {
-   private final TourneeRepository tourneeRepository;
+    private final TourneeRepository tourneeRepository;
     public TourneeEntity createTournee(TourneeEntity tourneeEntity){
 
         return tourneeRepository.save(tourneeEntity);
+    }
+    public List<TourneeEntity> getTourneesByDate(LocalDate date) {
+        // Convertir LocalDate en Date pour la requête
+        Date sqlDate = Date.valueOf(date);
+        return tourneeRepository.findByDateTournee(sqlDate);
     }
 }
